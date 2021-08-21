ef4954adb817586f2655cf4e292949c592036597
Bijan Etesam <bijanetesam@Bijan-MacBook-Air.local>
2021-08-21 22:15:18 +0300

    added cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -122,8 +122,6 @@ module.exports = {
           signed: true
         })
 
-        res.send(req.signedCookies)
-
         res.status(200).json({
           isAuth: true,
           isLoggedIn: true,
