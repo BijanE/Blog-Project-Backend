71b8ea2d34dab723df8cc7cbaff0c2f5bc8eb187
Bijan Etesam <bijanetesam@Bijan-MacBook-Air.local>
2021-08-22 00:53:51 +0300

    added cors options

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization'
